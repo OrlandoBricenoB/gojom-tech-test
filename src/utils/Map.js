fcca03915f194e3051552ae333fcc8f38144a217
orlandobriceno <orlando.briceno.blanco@gmail.com>
2022-10-22 14:01:38 -0400

    feat: Map.
    Generate map with centralGeoPoint or RestrictGeoBoundingBox.

---
+++ src/utils/Map.js
@@ -0,0 +1,17 @@
+import { getCentralGeoPoint, getRestrictGeoBoundingBox } from './Database'
+
+const Map = mapEl => {
+  const centralGeoPoint = getCentralGeoPoint()
+  const restrictGeoBoundingBox = getRestrictGeoBoundingBox()
+
+  return new google.maps.Map(mapEl, {
+    zoom: 10,
+    center: centralGeoPoint,
+    restriction: restrictGeoBoundingBox,
+    disableDefaultUI: true,
+    gestureHandling: 'greedy',
+    mapId: '4504f8b37365c3d0'
+  })
+}
+
+export default Map
