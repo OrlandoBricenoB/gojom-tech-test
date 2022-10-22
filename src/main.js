bedd249f7398a167a92226befe7334a768a0a569
orlandobriceno <orlando.briceno.blanco@gmail.com>
2022-10-22 13:55:50 -0400

    feat: Import initMap for declare global function.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,5 @@
 import './assets/css/tailwind.css'
 import './assets/css/index.css'
+import './assets/css/marker.css'
 
-const titleEl = document.querySelector('#title')
-
-if (titleEl) {
-  titleEl.innerHTML = 'Value'
-}
-
-// * Crear funciones para actualizar DOM, podría crear un DOM virtual...
-// * Amo crear vanilla, pero es más cómodo trabajar con React.
+import './utils/initMap'
