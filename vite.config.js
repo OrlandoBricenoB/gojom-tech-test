3d6d6788a5c11a51da33412d15218ebe14ab9dc1
orlandobriceno <orlando.briceno.blanco@gmail.com>
2022-10-24 05:29:57 -0400

    build: Install ESLint.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
+import eslint from 'vite-plugin-eslint'
 import sassDts from 'vite-plugin-sass-dts'
 
 export default defineConfig({
-  plugins: [sassDts({
-    enabledMode: ['development', 'production'],
-    global: {
-      generate: true
-    }
-  })]
+  plugins: [
+    eslint({
+      fix: true
+    }),
+    sassDts({
+      enabledMode: ['development', 'production'],
+      global: {
+        generate: true
+      }
+    })
+  ]
 })
